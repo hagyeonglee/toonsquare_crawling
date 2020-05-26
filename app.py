b4ec38810fd29d639763ab9769b8e72f0622bfa2
Lee Ha Gyeong <lhky0708@gmail.com>
2020-05-26 18:56:04 +0900

    update save to csv(supervised)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import *
 import pandas as pd
-
+import csv
 import numpy as np
 import sys
 from IPython.display import display, HTML
@@ -8,17 +8,17 @@ import ipywidgets as widgets
 
 app = Flask(__name__)
 listofemoji = []
-@app.route('/',methods=['GET','POST'])
-def make_read_csv():
-    data = pd.read_csv('naver_webtoon_comments.csv')
-    num_row = len(data)
+@app.route('/<int:num>',methods=['GET','POST'])
+def inputTest(num=None):
+    dataf = pd.read_csv('naver_webtoon_comments.csv')
+    num_row = len(dataf)
     s = pd.Series([index for index in range(1, num_row + 1)])
-    data.set_index([s], inplace=True)
-    data.index.name = None
-    data = data.drop(['Unnamed: 0'], axis=1)
-    print(data.shape)
-    print(request.method)
-    render_template('view.html', row_num=num_row, tables=[data.to_html()])
+    dataf.set_index([s], inplace=True)
+    dataf.index.name = None
+    dataf = dataf.drop(['Unnamed: 0'], axis=1)
+    dataf = dataf[(num-1)*15:(num)*15]
+    print(dataf.shape)
+    render_template('test.html', num=num, num_row=num_row,tables=[dataf.to_html()])
     if request.method == "POST":
         if request.form['submit_button'] == "중립":
             # pass
@@ -55,22 +55,66 @@ def make_read_csv():
             print("놀람")
             listofemoji.append("놀람")
             print(listofemoji)
-        # else:
-        #     return render_template('view.html', row_num=num_row, tables=[data.to_html()])
+        else:
+            render_template('test.html', num=num,num_row=num_row, tables=[dataf.to_html()])
     elif request.method == "GET":
-        print("get")
+        print("GET")
+    return render_template('test.html', num=num,num_row=num_row, tables=[dataf.to_html()])
+
+@app.route('/calculate',methods=['GET','POST'])
+def calculate(num=None):
+    if request.method == 'POST':
+        temp = request.form['num']
+    else:
+        temp = None
+    return redirect(url_for('inputTest',num=temp))
+
+
+
+@app.route('/',methods=['GET','POST'])
+def make_read_csv():
+    data = pd.read_csv('naver_webtoon_comments.csv')
+    num_row = len(data)
+    s = pd.Series([index for index in range(1, num_row + 1)])
+    data.set_index([s], inplace=True)
+    data.index.name = None
+    data = data.drop(['Unnamed: 0'], axis=1)
+    print(data.shape)
+    print(request.method)
     return render_template('view.html', row_num=num_row, tables=[data.to_html()])
 
-
-
 @app.route('/result',methods=['GET','POST'])
-def result():
-    # result = request.form
-    return render_template('supervise.html',result=result)
+def show():
+    if request.method=='POST':
+        request.form["toRESULT"] == 'toRESULT'
+    return redirect(url_for('showResult'))
 
-@app.route('/test')
-def test():
-    # result = request.form
-    return render_template('test.html')
+@app.route('/finalresult',methods=['GET','POST'])
+def showResult():
+    data = pd.read_csv('naver_webtoon_comments.csv')
+    num_row = len(data)
+    s = pd.Series([index for index in range(0, num_row)])
+    data.set_index([s], inplace=True)
+    data.index.name = None
+    data = data.drop(['Unnamed: 0'], axis=1)
+
+    data_emotion = pd.Series(listofemoji)
+    num_row = len(data_emotion)
+    data_emotion.index = [index for index in range(0, num_row)]
+    data_emotion.index.name = None
+    data_emotion = pd.DataFrame(columns=['supervised'], data=data_emotion)
+    print(data_emotion)
+    data['supervised'] = data_emotion['supervised']
+    if (request.method == "POST"):
+        if request.form['toCSV'] == 'submit':
+            print('submit')
+            data.reindex([index for index in range(0, num_row)])
+            data.index.name = None
+            data.to_csv('/home/ellie/Desktop/supervised.csv', sep=',', na_rep='NaN', quoting=csv.QUOTE_ALL)
+            print('check csv file')
+    return render_template('supervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()])
+
+# 개별 데이터를 바꾸고 싶으면 사용자가 index 입력하고 supervise 데이터 입력해야함 -> 따로 페이지를 만들어야할 듯
+
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)

19cb12f978a2774de1d8d78f9e656dbb0ec6738e
Lee Ha Gyeong <lhky0708@gmail.com>
2020-05-28 19:00:11 +0900

    complete v1.1

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import *
 import pandas as pd
 import csv
+import requests
 
 app = Flask(__name__)
 listofemoji = []
-
+arr = []
 
 @app.route('/<int:num>',methods=['GET','POST'])
 def inputTest(num=None):
@@ -16,7 +17,7 @@ def inputTest(num=None):
     dataf = dataf.drop(['Unnamed: 0'], axis=1)
     dataf = dataf[(num-1)*15:num * 15]
     print(dataf.shape)
-    render_template('test.html', num=num, num_row=num_row, tables=[dataf.to_html()])
+    render_template('selectview.html', num=num, num_row=num_row, tables=[dataf.to_html()])
     if request.method == "POST":
         if request.form['submit_button'] == "중립":
             # pass
@@ -54,10 +55,10 @@ def inputTest(num=None):
             listofemoji.append("놀람")
             print(listofemoji)
         else:
-            render_template('test.html', num=num,num_row=num_row, tables=[dataf.to_html()])
+            render_template('selectview.html', num=num, num_row=num_row, tables=[dataf.to_html()])
     elif request.method == "GET":
         print("GET")
-    return render_template('test.html', num=num,num_row=num_row, tables=[dataf.to_html()])
+    return render_template('selectview.html', num=num, num_row=num_row, tables=[dataf.to_html()])
 
 
 @app.route('/calculate',methods=['GET','POST'])
@@ -89,12 +90,29 @@ def make_read_csv():
     return render_template('view.html', row_num=num_row, tables=[data.to_html()])
 
 
+@app.route('/ffinal', methods=['GET', 'POST'])
+def showfinalResult():
+    data = pd.read_csv('supervised.csv')
+    num_row = len(data)
+    s = pd.Series([index for index in range(1, num_row + 1)])
+    data.set_index([s], inplace=True)
+    data.index.name = None
+    data = data.drop(['Unnamed: 0'], axis=1)
+    print(data.shape)
+    print(request.method)
+    return render_template('view.html', row_num=num_row, tables=[data.to_html()])
+
 @app.route('/result', methods=['GET', 'POST'])
 def show():
     if request.method=='POST':
         request.form["toRESULT"] == 'toRESULT'
     return redirect(url_for('showResult'))
 
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method=='POST':
+        request.form["edit"] == 'edit'
+    return redirect(url_for('editsupervise'))
 
 @app.route('/finalresult', methods=['GET', 'POST'])
 def showResult():
@@ -104,7 +122,6 @@ def showResult():
     data.set_index([s], inplace=True)
     data.index.name = None
     data = data.drop(['Unnamed: 0'], axis=1)
-
     data_emotion = pd.Series(listofemoji)
     num_row = len(data_emotion)
     data_emotion.index = [index for index in range(0, num_row)]
@@ -117,12 +134,58 @@ def showResult():
             print('submit')
             data.reindex([index for index in range(0, num_row)])
             data.index.name = None
-            data.to_csv('/home/ellie/Desktop/supervised.csv', sep=',', na_rep='NaN', quoting=csv.QUOTE_ALL)
+            data.to_csv('supervised.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
             print('check csv file')
     return render_template('supervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()])
 
 # 개별 데이터를 바꾸고 싶으면 사용자가 index 입력하고 supervise 데이터 입력해야함 -> 따로 페이지를 만들어야할 듯
+@app.route('/editsupervise',methods=['GET', 'POST'])
+def editsupervise():
+    print("get")
+    data = pd.read_csv('supervised.csv')
+    num_row = len(data)
+    s = pd.Series([index for index in range(1, num_row + 1)])
+    data.set_index([s], inplace=True)
+    data.index.name = None
+    data_emotion = data['supervised'].to_frame()
+    data = data.drop(['Unnamed: 0'], axis=1)
+    emotion = data_emotion.values
+    print(emotion)
+    # listofemoji = emotion
+    # print(listofemoji)
+    # print(listofemoji)
+    # print(data.shape)
+    print(request.method)
 
+    if request.method == "GET":
+        print(request.args.getlist('edata'))
+        arr = request.args.getlist('edata')
+        # data_emotion_new = arr
+        data_emotion_new = pd.Series(arr)
+        print(data_emotion_new)
+        num_row = len(data_emotion_new)
+        data_emotion_new.index = [index for index in range(1, num_row+1)]
+        data_emotion_new.index.name = None
+        data_emotion_new = pd.DataFrame(columns=['supervised'], data=data_emotion_new)
+        print(data_emotion_new)
+        data['supervised'] = data_emotion_new['supervised']
+        print('submit_get')
+        data.reindex([index for index in range(0, num_row)])
+        data.index.name = None
+        data.to_csv('supervised.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
+        print('check csv file_get')
+        data_emotion = data_emotion_new['supervised'].to_frame()
+
+        # redirect(url_for('editsupervise'))
+    # data = data.drop(['supervised'], axis=1)
+    elif request.method == "POST":
+        pass
+
+    return render_template('editsupervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()],newtables=[data_emotion_new.to_html()],
+                           emotiondata=emotion)
+
+
+    # /editsupervise 에서 save 버튼을 눌러야 supervised가 업데이트 됨.. 왜인지 못 찾음...
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)

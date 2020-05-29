c962730023119a4fc2f4425f5b066e224b5116da
Lee Ha Gyeong <lhky0708@gmail.com>
2020-05-29 14:17:13 +0900

    complete final (include md)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # toonsquare_crawling
+static
+   - style.css : 전체적인 css스타일 적용(표, 버튼 스타일)
+   - supervise.css : supervise하는 페이지의 css 스타일
+
+templates
+   - editsupervise.html : 수퍼바이즈한 데이터를 고치는 페이지에 대한 html 파일 : 1차적으로 수퍼바이즈가 완료된 데이터를 다시 고치고 싶을 때 보여지는 프론트 엔드 화면
+   - selectview.html : 1차 수퍼바이즈가 진행되는 화면 : 각 문장에서 느껴지는 감정에 해당하는 버튼을 클릭한 후 SAVE버튼을 무조건 눌러주어야만 저장이된다. 수퍼바이즈는 각 페이지 넘버를 입력하여 화면 이동하여 같은 방법으로 진행하면 된다.
+   - submitsup.html : 수퍼바이즈한 데이터를 고치는 페이지에 대한 html 파일 (2) : 수퍼바이즈 -> 수정 단계를 거친 최종의 supervised 데이터가 보여지느 프론트 화면
+   - supbervise.html : 1차 수퍼바이즈가 끝난 데이터들에 대해 결과를 보여주는 화면 역시, SAVE 버튼을 눌러야 저장이 되고 결과가 반영된다.(이때, csv파일로도 저장된다.)-> supervised_first.csv 라는 파일명으로 저장
+   - view.html : 홈 루트 페이지에서 보여지는 프론트 화면 이때, RESULT를 누르면 당연히 수퍼바이즈가 이뤄지지 않았으므로 supervised칸이 NaN으로 표시된다.
+   
+app.py : flask 앱 작동 -> 전체적인 대쉬보드 기능 구현
+
+naver_webtoon_comments.csv -> 크롤링한 네이버 댓글 데이터를 인공지능이 한 번 감정 분석을 함.
+
+supervised_first.csv -> 1차 수퍼바이즈한 데이터파일
+ 
+supervised.csv -> 최종 수퍼바이즈 데이터 파일
--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import pandas as pd
 import csv
 
 app = Flask(__name__)
+# 1차 수퍼바이즈한 감정들의 정보가 저장되는 리스트
 listofemoji = []
+# 1차 수퍼바이즈를 하고난 다음, 고쳐진 감정들의 정보가 저장되는 리스트
 arr = []
 
+# 네이버 웹툰에서 크롤링한 베스트 댓글을 15개 단위로 나누어 한 페이지에 보여주는 함수
+# 만약 csv 파일의 댓글 개수가 120개라면 1부터 8페이지까지 존재/ 1페이지는 page start 라고 표시되고, 8페이지는 page end라고 알려준다.
 @app.route('/<int:num>',methods=['GET','POST'])
 def inputTest(num=None):
     dataf = pd.read_csv('naver_webtoon_comments.csv')
@@ -58,8 +62,8 @@ def inputTest(num=None):
     elif request.method == "GET":
         print("GET")
     return render_template('selectview.html', num=num, num_row=num_row, tables=[dataf.to_html()])
-
-
+# 댓글을 보여주는 페이지의 숫자를 post 방식으로 inputTest()함수에게 넘겨주는 역할을 하는 함수
+# URL인 /calculate는 보여지는 않는다.
 @app.route('/calculate',methods=['GET','POST'])
 def calculate():
     if request.method == 'POST':
@@ -69,14 +73,8 @@ def calculate():
     return redirect(url_for('inputTest', num=temp))
 
 
-# 기본 홈(/루트)으로 가는 버튼
-@app.route('/tohome', methods=['GET', 'POST'])
-def showhome():
-    if request.method == 'POST':
-        request.form["HOME"] == 'HOME'
-    return redirect(url_for('make_read_csv'))
-
-
+# naver_webtoon_comments.csv 파일을 dataframe형식으로 보여주는 함수
+# home 페이지라고 생각하면 된다.
 @app.route('/', methods=['GET', 'POST'])
 def make_read_csv():
     data = pd.read_csv('naver_webtoon_comments.csv')
@@ -88,19 +86,16 @@ def make_read_csv():
     print(data.shape)
     print(request.method)
     return render_template('view.html', row_num=num_row, tables=[data.to_html()])
+# 크롤링한 파일을 dataframe형식으로 보여주는 home 루트로 갈 수 있도록 make_read_csv()함수로 리다이렉트된다.
+# URL인 /tohome 보여지는 않는다.
+@app.route('/tohome', methods=['GET', 'POST'])
+def showhome():
+    if request.method == 'POST':
+        request.form["HOME"] == 'HOME'
+    return redirect(url_for('make_read_csv'))
 
-@app.route('/result', methods=['GET', 'POST'])
-def show():
-    if request.method=='POST':
-        request.form["toRESULT"] == 'toRESULT'
-    return redirect(url_for('showResult'))
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
-    if request.method=='POST':
-        request.form["edit"] == 'edit'
-    return redirect(url_for('editsupervise'))
-
+# 수퍼바이즈가 모두 완료된 최종 결과를 화면에 보여준다. 앞선 수퍼바이즈한 결과, 수퍼바이즈를 수정한 결과 모두 post의 방법으로 받아온다.
 @app.route('/finalresult', methods=['GET', 'POST'])
 def showResult():
     data = pd.read_csv('naver_webtoon_comments.csv')
@@ -124,16 +119,15 @@ def showResult():
             data.to_csv('supervised.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
             print('check csv file')
     return render_template('supervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()])
+# showResult() 함수로 리다이렉트되고, result 버튼을 눌렀을 때 작동한다.
+# URL인 /result 보여지는 않는다.
+@app.route('/result', methods=['GET', 'POST'])
+def show():
+    if request.method=='POST':
+        request.form["toRESULT"] == 'toRESULT'
+    return redirect(url_for('showResult'))
 
-
-# 개별 데이터를 바꾸고 싶으면 사용자가 index 입력하고 supervise 데이터 입력해야함 -> 따로 페이지를 만들어야할 듯
-@app.route('/toresultedit', methods=['GET', 'POST'])
-def toresultedit():
-    if request.method == "POST":
-        request.form['SAVE'] == 'save'
-    return redirect(url_for('resultsup'))
-
-
+# 1차 수퍼바이즈된 결과에서 사용자가 다시 input창에 넣어 보여주고 데이터를 수정한다.
 @app.route('/editsupervise',methods=['GET', 'POST'])
 def editsupervise():
     print("get")
@@ -146,10 +140,6 @@ def editsupervise():
     data = data.drop(['abc'], axis=1)
     emotion = data_emotion.values
     print(emotion)
-    # listofemoji = emotion
-    # print(listofemoji)
-    # print(listofemoji)
-    # print(data.shape)
     print(request.method)
 
     if request.method == "GET":
@@ -167,26 +157,41 @@ def editsupervise():
         print('submit_get')
         data = data.drop(['abc'], axis=1)
         # data = data.drop(['supervised'])
-        data.to_csv('supervised_final.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
+        data.to_csv('supervised_first.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
         # print('check csv file_get')
         data_emotion = data_emotion_new['supervised'].to_frame()
         print(data.head())
-    return render_template('editsupervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()],newtables=[data_emotion_new.to_html()],
+    return render_template('editsupervise.html', tables=[data_emotion.to_html()], datatables=[data.to_html()], newtables=[data_emotion_new.to_html()],
                            emotiondata=emotion)
+# editsupervise() 함수로 리다이렉트되고, edit 버튼을 눌렀을 때 작동한다.
+# URL인 /edit 보여지는 않는다.
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method=='POST':
+        request.form["edit"] == 'edit'
+    return redirect(url_for('editsupervise'))
 
+
+# 1차 수퍼바이즈된 결과에서 사용자가 다시 input창에 넣어 수정한 데이터의 결과를 가져오고 리턴해준다.
 @app.route('/resultsup', methods=['GET', 'POST'])
 def resultsup():
-    data_emotion = pd.read_csv('supervised_final.csv')
+    data_emotion = pd.read_csv('supervised_first.csv')
     s = pd.Series([index for index in range(0, len(data_emotion))])
     data_emotion.set_index([s], inplace=True)
     data_emotion.index.name = None
     data_emotion = data_emotion.drop(['Unnamed: 0'], axis=1)
-# redirect(url_for('editsupervise'))
-# data = data.drop(['supervised'], axis=1)
     data = data_emotion.drop(['supervised'], axis=1)
     data.to_csv('supervised.csv', sep=',', na_rep="None", quoting=csv.QUOTE_ALL)
     print('check csv file2')
     return render_template('submitsup.html', tables=[data_emotion.to_html()])
+# resultsup() 함수로 리다이렉트되고, save 버튼을 눌렀을 때 작동한다.
+# URL인 /toreulstedit 보여지는 않는다.
+@app.route('/toresultedit', methods=['GET', 'POST'])
+def toresultedit():
+    if request.method == "POST":
+        request.form['SAVE'] == 'save'
+    return redirect(url_for('resultsup'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
